7fb90166152a5135ca5a7eadb247fa64759bdb36
kulurumu <muralidhar.kuluru@engelvoelkers.com>
2026-05-26 19:58:12 +0200

    Initial Commit

---
+++ CryptoCurrencyTracker/CryptoCurrencyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyTrackerApp.swift
+//  CryptoCurrencyTracker
+//
+//  Created by Kuluru, Muralidhar on 26.05.26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
